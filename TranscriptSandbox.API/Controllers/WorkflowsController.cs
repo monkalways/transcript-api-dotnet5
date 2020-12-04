31581ddad0fa2fe240d06ef4eee7badf35e24c77
Wei.Wei <wei.wei@gov.ab.ca>
2020-12-04 15:32:59 -0700

    Initial commit

---
+++ TranscriptSandbox.API/Controllers/WorkflowsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TranscriptSandbox.API.Workflows;
+using WorkflowCore.Interface;
+
+namespace TranscriptSandbox.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkflowsController : ControllerBase
+    {
+        public WorkflowsController(IWorkflowController workflowService)
+        {
+            WorkflowService = workflowService;
+        }
+
+        public IWorkflowController WorkflowService { get; }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var workflowId = await WorkflowService.StartWorkflow("TestWorkflow");
+            return $"TestWorkflow ({workflowId}) started";
+        }
+    }
+}
---
+++ TranscriptSandbox.API/Workflows/TestWorkflow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace TranscriptSandbox.API.Workflows
+{
+    public class TestWorkflow : IWorkflow
+    {
+        public string Id => "TestWorkflow";
+
+        public int Version => 1;
+
+        public void Build(IWorkflowBuilder<object> builder)
+        {
+            builder
+            .StartWith(context =>
+            {
+                Console.WriteLine("Hello world");
+                return ExecutionResult.Next();
+            })
+            .Then(context =>
+            {
+                Console.WriteLine("Goodbye world");
+                return ExecutionResult.Next();
+            });
+        }
+    }
+}
